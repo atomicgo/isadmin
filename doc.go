2cf7ff4382419e421e110dfe143175f3cf5f388e
Marvin Wendt <marvin.wendt@mail.schwarz>
2024-01-16 15:01:57 +0100

    docs: added godoc

--- doc.go
+++ doc.go
@@ -1,9 +1,4 @@
 /*
-Package template is used to generate new AtomicGo repositories.
-
-Write the description of the module here. You can use **markdown**!
-This description should clearly explain what the package does.
-
-Example description: https://golang.org/src/encoding/gob/doc.go
+Package isadmin provides a simple way to check if the program has administrative privileges.
 */
 package isadmin
